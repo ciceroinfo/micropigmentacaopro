77e444be6e4ed2b1722382a6590da189b30a2a88
ciceroinfo <cicero.info@gmail.com>
2019-05-04 11:32:46 -0300

    added img zoom

--- scripts.js
+++ scripts.js
@@ -6,3 +6,28 @@ function clickMobileMenu() {
     x.style.display = "block";
   }
 }
+
+function clickImageModal(elmnt) {
+	// Get the modal
+	var modal = document.getElementById('myModal');
+
+	// Get the image and insert it inside the modal - use its "alt" text as a caption
+	//var img = document.getElementById('myImg');
+	var modalImg = document.getElementById("img01");
+	var captionText = document.getElementById("caption");
+
+
+	modal.style.display = "block";
+    modalImg.src = elmnt.src;
+    captionText.innerHTML = elmnt.alt;
+}
+
+function clickModalClose() {
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// Get the modal
+	var modal = document.getElementById('myModal');
+
+    modal.style.display = "none";
+}
